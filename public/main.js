38ace67d0d2e4125023308a72c525b22261c25ec
David B <shorerydr10@live.com>
2014-02-13 15:07:37 -0800

    first commit

---
+++ public/main.js
@@ -0,0 +1,26 @@
+// Init Skrollr
+
+
+
+$(function () {
+    $.scrollIt();
+});
+
+var headroom = new Headroom(header, {
+    "tolerance": 5,
+    "offset": 5,
+    "classes": {
+        "initial": "animated",
+        "pinned": "slideDown",
+        "unpinned": "slideUp"
+    }
+});
+headroom.init();
+
+ // to destroy
+headroom.destroy();
+$(document).ready(function () {
+    $('.menu-link').bigSlide();
+});
+
+$('.billboard h2').addClass('complete').delay(800);
